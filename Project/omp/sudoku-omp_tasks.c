413fcc4a0dd92538ad3866206ad1e4e61c82c407
Jguer <me@jguer.space>
2018-04-05 20:47:26 +0100

    use perror
    Signed-off-by: Jguer <me@jguer.space>

--- Project/omp/sudoku-omp_tasks.c
+++ Project/omp/sudoku-omp_tasks.c
@@ -38,11 +38,11 @@ typedef uint_fast8_t *sudoku;
 
 // Mother of all sudokus
 typedef struct moas_t {
-  square empty_sqs;
-  sudoku to_solve;
-  int_fast32_t n;
-  int_fast32_t box_size;
-  int_fast32_t n_empty_sq;
+    square empty_sqs;
+    sudoku to_solve;
+    int_fast32_t n;
+    int_fast32_t box_size;
+    int_fast32_t n_empty_sq;
 } moas;
 
 int gDONE;
@@ -50,196 +50,199 @@ int thread_count = 4;
 moas *gMOAS;
 
 void print_error(char *error) {
-  fprintf(stderr, error);
-  exit(EXIT_FAILURE);
+    perror(error);
+    exit(EXIT_FAILURE);
 }
 
 int read_file(const char *filename) {
-  FILE *sudoku_file;
-  int box_size;
-  uint8_t num;
+    FILE *sudoku_file;
+    int box_size;
+    uint8_t num;
 
-  /* Opens file */
-  sudoku_file = fopen(filename, "r");
-  if (sudoku_file == NULL)
-    print_error("Could not open file\n");
+    /* Opens file */
+    sudoku_file = fopen(filename, "r");
+    if (sudoku_file == NULL)
+        print_error("Could not open file\n");
 
-  /* Scans first line (aka the square size) */
-  if (fscanf(sudoku_file, "%d", &box_size) == EOF)
-    print_error("Could not read file\n");
+    /* Scans first line (aka the square size) */
+    if (fscanf(sudoku_file, "%d", &box_size) == EOF)
+        print_error("Could not read file\n");
 
-  gMOAS = (moas *)malloc(sizeof(moas));
-  if (gMOAS == NULL) {
-    print_error("Unable to init MOAS");
-  }
-
-  gMOAS->box_size = box_size;
-  gMOAS->n = box_size * box_size;
-
-  gMOAS->to_solve = (sudoku)malloc(gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
-
-  /* Read the file */
-  uint_fast32_t iter = 0;
-  for (int i = 0; i < gMOAS->n * gMOAS->n; i++) {
-    fscanf(sudoku_file, "%2" SCNu8, &num);
-    gMOAS->to_solve[i] = num;
-    if (num == 0) {
-      iter++;
+    gMOAS = (moas *)malloc(sizeof(moas));
+    if (gMOAS == NULL) {
+        print_error("Unable to init MOAS");
     }
-  }
 
-  if (iter == 0) {
+    gMOAS->box_size = box_size;
+    gMOAS->n = box_size * box_size;
+
+    gMOAS->to_solve = (sudoku)malloc(gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
+
+    /* Read the file */
+    uint_fast32_t iter = 0;
+    for (int i = 0; i < gMOAS->n * gMOAS->n; i++) {
+        fscanf(sudoku_file, "%2" SCNu8, &num);
+        gMOAS->to_solve[i] = num;
+        if (num == 0) {
+            iter++;
+        }
+    }
+
+    if (iter == 0) {
+        fclose(sudoku_file);
+        return 1;
+    }
+
+    // Create aux array of empty squares
+    gMOAS->n_empty_sq = iter;
+    gMOAS->empty_sqs = (square)malloc(iter * sizeof(uint_fast8_t));
+
+    for (int i = 0, itera = 0; i < gMOAS->n * gMOAS->n; i++) {
+        if (gMOAS->to_solve[i] == 0) {
+            gMOAS->empty_sqs[itera] = i;
+            itera++;
+        }
+    }
+
     fclose(sudoku_file);
-    return 1;
-  }
-
-  // Create aux array of empty squares
-  gMOAS->n_empty_sq = iter;
-  gMOAS->empty_sqs = (square)malloc(iter * sizeof(uint_fast8_t));
-
-  for (int i = 0, itera = 0; i < gMOAS->n * gMOAS->n; i++) {
-    if (gMOAS->to_solve[i] == 0) {
-      gMOAS->empty_sqs[itera] = i;
-      itera++;
-    }
-  }
-
-  fclose(sudoku_file);
-  return 0;
+    return 0;
 }
 
 // Free sudoku typedef
-void free_sudoku(sudoku to_free) { free(to_free); }
+void free_sudoku(sudoku to_free) {
+    free(to_free);
+}
 
 // Free global MOAS
 void free_gMOAS() {
-  free_sudoku(gMOAS->to_solve);
-  free(gMOAS->empty_sqs);
+    free_sudoku(gMOAS->to_solve);
+    free(gMOAS->empty_sqs);
 
-  free(gMOAS);
+    free(gMOAS);
 }
 
 // Print grid
 void print_grid(sudoku to_print) {
-  for (int i = 0; i < gMOAS->n; i++) {
-    for (int j = 0; j < gMOAS->n; j++) {
-      printf("%2d ", to_print[i * gMOAS->n + j]);
+    for (int i = 0; i < gMOAS->n; i++) {
+        for (int j = 0; j < gMOAS->n; j++) {
+            printf("%2d ", to_print[i * gMOAS->n + j]);
+        }
+        printf("\n");
     }
     printf("\n");
-  }
-  printf("\n");
 }
 
 // Check box, row and column for safety
 bool safe(sudoku to_check, int id, int num) {
-  int x = id / gMOAS->n, y = id % gMOAS->n;
+    int x = id / gMOAS->n, y = id % gMOAS->n;
 
-  int bx = (x / gMOAS->box_size) * gMOAS->box_size,
-      by = (y / gMOAS->box_size) * gMOAS->box_size;
-  int j, ox, oy;
-  for (j = 0; j < gMOAS->n; j++) {
-    ox = j / gMOAS->box_size;
-    oy = j % gMOAS->box_size;
-    // check row
-    if (y != j && num == to_check[x * gMOAS->n + j])
-      return false;
-    // check column
-    if (x != j && num == to_check[j * gMOAS->n + y])
-      return false;
-    // check box
-    if (id != ((bx + ox) * gMOAS->n + by + oy) &&
-        num == to_check[(bx + ox) * gMOAS->n + by + oy])
-      return false;
-  }
-  return true;
+    int bx = (x / gMOAS->box_size) * gMOAS->box_size,
+        by = (y / gMOAS->box_size) * gMOAS->box_size;
+    int j, ox, oy;
+    for (j = 0; j < gMOAS->n; j++) {
+        ox = j / gMOAS->box_size;
+        oy = j % gMOAS->box_size;
+        // check row
+        if (y != j && num == to_check[x * gMOAS->n + j])
+            return false;
+        // check column
+        if (x != j && num == to_check[j * gMOAS->n + y])
+            return false;
+        // check box
+        if (id != ((bx + ox) * gMOAS->n + by + oy) &&
+                num == to_check[(bx + ox) * gMOAS->n + by + oy])
+            return false;
+    }
+    return true;
 }
 
 void copy_to_gMOAS(sudoku solved) {
-  for (int i = 0; i < gMOAS->n_empty_sq; i++) {
-    gMOAS->to_solve[gMOAS->empty_sqs[i]] = solved[gMOAS->empty_sqs[i]];
-  }
+    for (int i = 0; i < gMOAS->n_empty_sq; i++) {
+        gMOAS->to_solve[gMOAS->empty_sqs[i]] = solved[gMOAS->empty_sqs[i]];
+    }
 }
 
 sudoku new_state_copy(sudoku old_state) {
-  sudoku new_state = (sudoku)malloc(gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
-  memcpy(new_state, old_state, gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
-  return new_state;
+    sudoku new_state = (sudoku)malloc(gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
+    memcpy(new_state, old_state, gMOAS->n * gMOAS->n * sizeof(uint_fast8_t));
+    return new_state;
 }
 
 void solve(int id, sudoku state) {
-  if (gDONE) {
-    return;
-  }
-
-  /* if in the last solve layer check if solved and start popping */
-  if (id == gMOAS->n_empty_sq - 1) {
-    for (int i = 1; i <= gMOAS->n; i++) {
-      if (safe(state, gMOAS->empty_sqs[id], i)) {
-        state[gMOAS->empty_sqs[id]] = i;
-        copy_to_gMOAS(state);
-        gDONE = true;
+    if (gDONE) {
         return;
-      }
     }
+
+    /* if in the last solve layer check if solved and start popping */
+    if (id == gMOAS->n_empty_sq - 1) {
+        for (int i = 1; i <= gMOAS->n; i++) {
+            if (safe(state, gMOAS->empty_sqs[id], i)) {
+                state[gMOAS->empty_sqs[id]] = i;
+                copy_to_gMOAS(state);
+                gDONE = true;
+                return;
+            }
+        }
+        free_sudoku(state);
+        return;
+    }
+
+    /* try each son solution */
+
+    for (int i = 1; i <= gMOAS->n; i++) {
+        #pragma omp task shared(state) firstprivate(i, id) untied
+        {
+            if (safe(state, gMOAS->empty_sqs[id], i)) {
+                sudoku new_state = new_state_copy(state);
+                new_state[gMOAS->empty_sqs[id]] = i;
+                solve(id + 1, new_state);
+            }
+        }
+    }
+    #pragma omp taskwait
+    #pragma omp single nowait
     free_sudoku(state);
-    return;
-  }
-
-  /* try each son solution */
-
-  for (int i = 1; i <= gMOAS->n; i++) {
-#pragma omp task shared(state) firstprivate(i, id) untied
-    {
-      if (safe(state, gMOAS->empty_sqs[id], i)) {
-        sudoku new_state = new_state_copy(state);
-        new_state[gMOAS->empty_sqs[id]] = i;
-        solve(id + 1, new_state);
-      }
-    }
-  }
-#pragma omp taskwait
-  free_sudoku(state);
 }
 
 int main(int argc, char const *argv[]) {
-  if (argc < N_ARGS) {
-    char error[64];
-    sprintf(error, "Usage: %s filename\n", argv[0]);
-    print_error(error);
-  } else if (argc == 3) {
-    thread_count = atoi(argv[2]);
-  }
+    if (argc < N_ARGS) {
+        char error[64];
+        sprintf(error, "Usage: %s filename\n", argv[0]);
+        print_error(error);
+    } else if (argc == 3) {
+        thread_count = atoi(argv[2]);
+    }
 
-  if (read_file(argv[1]) != 0) {
-    char error[64];
-    sprintf(error, "Unable to read file %s\n", argv[1]);
-    print_error(error);
-  }
+    if (read_file(argv[1]) != 0) {
+        char error[64];
+        sprintf(error, "Unable to read file %s\n", argv[1]);
+        print_error(error);
+    }
 
-  puts("~~~ Input Sudoku ~~~");
-  print_grid(gMOAS->to_solve);
-
-  // Solve the puzzle
-  sudoku new_state = new_state_copy(gMOAS->to_solve);
-  omp_set_num_threads(thread_count);
-  double start = omp_get_wtime();
-#pragma omp parallel
-  {
-#pragma omp single
-    solve(0, new_state);
-  }
-  double finish = omp_get_wtime();
-  if (gDONE) {
-    puts("~~~ Output Sudoku ~~~");
+    puts("~~~ Input Sudoku ~~~");
     print_grid(gMOAS->to_solve);
-    printf("Solved Sudoku\n");
-    // Solution found
-  } else {
-    printf("Did not solve Sudoku\n");
-    // No solution
-  };
 
-  free_gMOAS();
-  printf("Total Time %lfs\n", (double)(finish - start));
-  return 0;
+    // Solve the puzzle
+    sudoku new_state = new_state_copy(gMOAS->to_solve);
+    omp_set_num_threads(thread_count);
+    double start = omp_get_wtime();
+    #pragma omp parallel
+    {
+        #pragma omp single
+        solve(0, new_state);
+    }
+    double finish = omp_get_wtime();
+    if (gDONE) {
+        puts("~~~ Output Sudoku ~~~");
+        print_grid(gMOAS->to_solve);
+        printf("Solved Sudoku\n");
+        // Solution found
+    } else {
+        printf("Did not solve Sudoku\n");
+        // No solution
+    };
+
+    free_gMOAS();
+    printf("Total Time %lfs\n", (double)(finish - start));
+    return 0;
 }
