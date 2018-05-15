d2cd1ae559ec08fa78f31b46713a1652cebeb824
miguelpinho <33704675+miguelpinho@users.noreply.github.com>
2018-05-15 15:18:43 +0100

    bug in main.c, datatype related

--- Project2/mpi/main.c
+++ Project2/mpi/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #define INIT_TAG 1
 #define DIE_TAG 2
@@ -20,15 +22,18 @@ typedef struct moas {
 moas_t *gMOAS;
 
 void exit_colony(int ntasks) {
-    for (int id = 1; id < ntasks; ++id) {
+    int id;
+
+    for (id = 1; id < ntasks; ++id) {
         MPI_Send(0, 0, MPI_INT, id, DIE_TAG, MPI_COMM_WORLD);
     }
-    exit(1);
 }
 
 void print_grid() {
-    for (int x = 0; x < gMOAS->n; x++) {
-        for (int y = 0; y < gMOAS->n; y++) {
+    int x, y;
+
+    for (x = 0; x < gMOAS->n; x++) {
+        for (y = 0; y < gMOAS->n; y++) {
             if (gMOAS->known[x][y]) {
                 printf("%2d ", gMOAS->known[x][y]);
             } else {
@@ -44,6 +49,7 @@ void read_file(const char *filename, int ntasks) {
     FILE *sudoku_file;
     int box_size;
     uint8_t num;
+    int x, y, id, n;
 
     sudoku_file = fopen(filename, "re");
     if (sudoku_file == NULL) {
@@ -64,7 +70,7 @@ void read_file(const char *filename, int ntasks) {
     gMOAS->n = box_size * box_size;
     /* Init our bit mask */
     gMOAS->bits = (int *)calloc(gMOAS->n + 1, sizeof(int));
-    for (int n = 1; n < gMOAS->n + 1; n++) {
+    for (n = 1; n < gMOAS->n + 1; n++) {
         gMOAS->bits[n] = 1 << n;
     }
 
@@ -74,12 +80,12 @@ void read_file(const char *filename, int ntasks) {
     gMOAS->rows = (int *)calloc(gMOAS->n, sizeof(int));
     gMOAS->cols = (int *)calloc(gMOAS->n, sizeof(int));
     gMOAS->squares = (int *)calloc(gMOAS->n, sizeof(int));
-    for (int x = 0; x < gMOAS->n; x++) {
+    for (x = 0; x < gMOAS->n; x++) {
         gMOAS->known[x] = (int *)calloc(gMOAS->n, sizeof(int));
         gMOAS->grid[x] = (int *)calloc(gMOAS->n, sizeof(int));
-        for (int y = 0; y < gMOAS->n; y++) {
+        for (y = 0; y < gMOAS->n; y++) {
             fscanf(sudoku_file, "%2" SCNu8, &num);
-            set_cell(x, y, num);
+            //set_cell(x, y, num);
             gMOAS->known[x][y] = num;
             for (id = 1; id < ntasks; ++id) {
                 MPI_Send(&num, 1, MPI_INT, id, INIT_TAG, MPI_COMM_WORLD);
@@ -88,12 +94,12 @@ void read_file(const char *filename, int ntasks) {
     }
 
     fclose(sudoku_file);
-    return 0;
 }
 
 void build_map() {
 
     MPI_Status status;
+    int x, y, n, msg;
 
     gMOAS = (moas_t *)malloc(sizeof(moas_t));
     MPI_Recv(&msg, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -101,7 +107,7 @@ void build_map() {
     gMOAS->n = msg * msg;
     /* Init our bit mask */
     gMOAS->bits = (int *)calloc(gMOAS->n + 1, sizeof(int));
-    for (int n = 1; n < gMOAS->n + 1; n++) {
+    for (n = 1; n < gMOAS->n + 1; n++) {
         gMOAS->bits[n] = 1 << n;
     }
 
@@ -110,17 +116,15 @@ void build_map() {
     gMOAS->rows = (int *)calloc(gMOAS->n, sizeof(int));
     gMOAS->cols = (int *)calloc(gMOAS->n, sizeof(int));
     gMOAS->squares = (int *)calloc(gMOAS->n, sizeof(int));
-    for (int x = 0; x < gMOAS->n; x++) {
+    for (x = 0; x < gMOAS->n; x++) {
         gMOAS->known[x] = (int *)calloc(gMOAS->n, sizeof(int));
         gMOAS->grid[x] = (int *)calloc(gMOAS->n, sizeof(int));
-        for (int y = 0; y < gMOAS->n; y++) {
+        for (y = 0; y < gMOAS->n; y++) {
             MPI_Recv(&msg, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            set_cell(x, y, msg);
+            //set_cell(x, y, msg);
             gMOAS->known[x][y] = msg;
         }
     }
-
-    return 0;
 }
 
 
@@ -131,7 +135,9 @@ void master(){
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
 
     // Read file and send to slaves
-    read_file("4x4.txt", ntasks);
+    read_file("testfiles/4x4.txt", ntasks);
+
+    print_grid();
 
     // Send exit signal 
     exit_colony(ntasks);
@@ -141,7 +147,10 @@ void slave() {
 
     MPI_Status status;
     int msg;
+
     build_map();
+    print_grid();
+
     while(1) {
         MPI_Recv(&msg, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     
@@ -151,8 +160,6 @@ void slave() {
         else if(status.MPI_TAG == WORK_TAG) {
             // Here we work on the task or else we do an interrupt
         }
-
-        print_grid();
     }
 }
 
