880d6c9ba62a3b43ef679e17215809758eee3906
Jguer <me@jguer.space>
2018-06-09 19:49:29 +0100

    1516E2 real one

--- Labs/1516E2/1.c
+++ Labs/1516E2/1.c
@@ -1,14 +1,24 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define N 100
 
 int main(int argc, char const *argv[]) {
-    omp_set_num_threads(2);
-    int i;
+    omp_set_num_threads(8);
+    int i, sum = 0;
+    int a[N] = {0};
 
-    #pragma omp parallel for schedule(dynamic, 2)
-    for (i = 0; i < 6; i++) {
-        printf("%i, %i\n", omp_get_thread_num(), i);
-    }
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++)
+        a[i] = i;
+
+    #pragma omp parallel for
+    for (i = 0; i < N; ++i)
+        sum += a[i];
+
+    printf("sum: %d\n", sum);
     return 0;
 }
