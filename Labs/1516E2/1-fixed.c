7631b82c13544230efc4c7a1ab77189d7c525609
Jguer <me@jguer.space>
2018-06-09 19:50:44 +0100

    1516E2 1-fixed

---
+++ Labs/1516E2/1-fixed.c
@@ -0,0 +1,24 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 100
+
+int main(int argc, char const *argv[]) {
+    omp_set_num_threads(8);
+    int i, sum = 0;
+    int a[N] = {0};
+
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++)
+        a[i] = i;
+
+    #pragma omp parallel for reduction(+ : sum)
+    for (i = 0; i < N; ++i)
+        sum += a[i];
+
+    printf("sum: %d\n", sum);
+    return 0;
+}
