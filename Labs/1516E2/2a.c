f3e4050e81b5a0dceed04592f0663a78e3691167
Jguer <me@jguer.space>
2018-06-09 20:27:49 +0100

    2b

--- Labs/1516E2/2a.c
+++ Labs/1516E2/2a.c
@@ -8,15 +8,17 @@ void hadamard(int **A, int **B, int **C, int N, int M) {
     for (i = 0; i < N; ++i) {
         int j;
         #pragma omp parallel for schedule(static, 20)
-        for (j = 0; j < M; ++j)
+        for (j = 0; j < M; ++j) {
+            printf("T%d on [%d][%d]\n", omp_get_thread_num(), i, j);
             C[i][j] = A[i][j] * B[i][j];
+        }
     }
 }
 
 int main(int argc, char const *argv[]) {
     omp_set_num_threads(4);
-    int M = 10000;
-    int N = 15000;
+    int M = 50;
+    int N = 50;
     int **a = malloc(sizeof(int *) * N);
     int **b = malloc(sizeof(int *) * N);
     int **c = malloc(sizeof(int *) * N);
